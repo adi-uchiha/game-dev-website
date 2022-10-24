0171a3d6cdda3d3f3e9985c18f4c9c2b01fbd902
Aditya Shelke <adi.cs.night@gmail.com>
2022-10-24 18:04:11 +0530

    logic for password-eye

--- pages/auth/login/index.jsx
+++ pages/auth/login/index.jsx
@@ -10,7 +10,7 @@ const Login = () => {
             <Script src="https://kit.fontawesome.com/90915ad547.js" crossOrigin="anonymous"></Script>
             <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
             <Tilt
-            className="card lg:h-[40vh] lg:w-[40vw] md:h-[40vh] md:w-[60vw] h-[40vh] w-[90vw] flex Tilt bg-slate-500/10 rounded-xl backdrop-blur"
+            className="card lg:h-[40vh] lg:w-[45vw] md:h-[40vh] md:w-[60vw] h-[40vh] w-[90vw] flex Tilt bg-slate-500/10 rounded-xl backdrop-blur"
             options={{ max: 25 }} >
                 <div className="Tilt-inner">
                     <AuthCard/>
--- pages/auth/register/index.jsx
+++ pages/auth/register/index.jsx
@@ -2,18 +2,18 @@ import Script from 'next/script'
 import React from 'react'
 import Tilt from 'react-tilt'
 import IconBackground from '../../../components/IconBackground'
-import AuthCard from '../../../components/AuthCard'
+import AuthCard2 from '../../../components/AuthCard2'
 
 const index = () => {
   return (
-        <div>
+        <div className='w-full h-[100vh]'>
             <Script src="https://kit.fontawesome.com/90915ad547.js" crossOrigin="anonymous"></Script>
             <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
             <Tilt
-            className="card lg:h-[40vh] lg:w-[40vw] md:h-[40vh] md:w-[60vw] h-[40vh] w-[90vw] flex Tilt bg-slate-500/10 rounded-xl backdrop-blur"
+            className="card lg:h-[40vh] lg:w-[45vw] md:h-[40vh] md:w-[60vw] h-[40vh] w-[85vw] flex Tilt bg-slate-500/10 rounded-xl backdrop-blur"
             options={{ max: 25 }} >
                 <div className="Tilt-inner">
-                    <AuthCard type='register'/>
+                    <AuthCard2 type='register'/>
                 </div>
             </Tilt>
             </div>
