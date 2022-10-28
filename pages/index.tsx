48da06a9f4fa6948c2100e57caafa8a9e8f1793e
Aditya Shelke <adi.cs.night@gmail.com>
2022-10-28 16:15:13 +0530

    Basic fetch of igdb by SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import axios from 'axios';
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Script from 'next/script'
+import { useEffect } from 'react';
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
-const Home: NextPage = () => {
+
+const Home: NextPage = (result) => {
+
+console.log(result)
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Header />
@@ -14,7 +19,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        
+        <img src="https://images.igdb.com/igdb/image/upload/t_screenshot_med_2x/dfgkfivjrhcksyymh9vw.jpg" alt="" />
       </main>
 
      <Footer />
@@ -23,3 +28,39 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getServerSideProps() {
+
+  var data = ''
+  
+  const res = await axios({
+    url: "https://api.igdb.com/v4/artworks",
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Client-ID': 'k7sugbi2a293oh0k9n2qcnn87zh50o',
+        'Authorization': 'Bearer o4yohw615qr1vg2v39jfy50gxnseys',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials':'*',
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
+    },
+    data: "fields *;"
+  })
+    .then(response => {
+        console.log(response.data);
+        data = response.data
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+
+
+
+  return {
+    props: {
+      result: data
+    }
+  }
+}
