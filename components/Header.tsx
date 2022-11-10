0daa9442c00b4c21fcfcb22cd37dcb8accaefa95
Aditya Shelke <adi.cs.night@gmail.com>
2022-11-11 02:03:05 +0530

    Backdrop gradient responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='w-[100vw] justify-start bg-opacity-40
       h-[8vh] z-20
-     bg-gray-600 fixed backdrop-blur-md text-white'>
+     bg-gray-600 fixed backdrop-blur-sm text-white'>
       <div className='flex '>
         <div className='mr-[10%]'>Logo</div>
         <div className='flex space-x-10 justify-start'>
--- components/home/Backdrop.tsx
+++ components/home/Backdrop.tsx
@@ -11,22 +11,22 @@ const Backdrop = ({ random }: props) => {
     const randomUrl = ImageUrls[random]
 
     const backdropImage = 
-    <div className='backdropImage top-[6rem] left-24 
-    lg:w-[98.7vw] md:w-[100vw] w-[100vw] -z-20' >
-    <div className='w-full h-[100vh] pl-[8%] pt-[10vh] relative 
-    lg:top-[4.5vw] md:top-[8vw] top-[12vw] lg:left-[10vw] md:left-[8vw]' >
+    <div className='backdropImage backdropImage top-[6rem] left-24 w-[100vw] -z-20 
+    '>
+    <div className='w-full h-[100vh] pl-[8%] pt-[10vh] relative
+    top-[8vh] lg:left-[15vw]' >
         <Image
             className='-z-10'
             src={randomUrl} alt=""
             priority
-            layout="fill" 
+            layout="fill"
             objectFit="cover"
             />
             </div>
     </div>
 
     return (
-        <div className='' >
+        <div className='overflow-clip' >
             {randomUrl ?
                  backdropImage  :
                 <h1>Loading</h1>
